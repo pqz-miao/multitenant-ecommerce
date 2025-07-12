9e195b9f99ef96cf1ec845d9138d0cd552fad1f7
miao-pqz <1125583676.qq.com>
2025-07-12 10:16:07 +0800

    04: payload integration

---
+++ src/app/(app)/(home)/page.tsx
@@ -0,0 +1,19 @@
+import { getPayload } from "payload";
+
+import configPromise from "@payload-config";
+
+export default async function Home() {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+
+  const data = await payload.find({
+    collection: "users",
+  });
+
+  return (
+    <div>
+      {JSON.stringify(data, null, 2)}
+    </div>
+  );
+}
