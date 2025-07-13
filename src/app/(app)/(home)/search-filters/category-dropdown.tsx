6b4c60e667e75a34a599dac35e9b1a42c4cc54ed
miao-pqz <1125583676.qq.com>
2025-07-13 17:53:35 +0800

    06: categories finalization

--- src/app/(app)/(home)/search-filters/category-dropdown.tsx
+++ src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -1,17 +1,18 @@
 "use client";
 
+import Link from "next/link";
 import { useRef, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 
 import { cn } from "@/lib/utils";
-import { Category } from "@/payload-types";
 
+import { CustomCategory } from "../types";
 import { SubcategoryMenu } from "./subcategory-menu";
 import { useDropdownPosition } from "./use-dropdown-position";
 
 interface Props {
-    category: Category;
+    category: CustomCategory;
     isActive?: boolean;
     isNavigationHovered?: boolean;
 };
@@ -47,9 +48,14 @@ export const CategoryDropdown = ({
                     className={cn(
                         "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
                         isActive && !isNavigationHovered && "bg-white border-primary",
+                        isOpen && "bg-white border-primary shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[4px] -translate-y-[4px]"
                     )}
                 >
-                    {category.name}
+                    <Link
+                        href={`/${category.slug === "all" ? "" : category.slug}`}
+                    >
+                        {category.name}
+                    </Link>
                 </Button>
                 {category.subcategories && category.subcategories.length > 0 && (
                     <div
