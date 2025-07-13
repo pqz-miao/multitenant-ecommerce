1c105aad9ad40abac477f9850f9d67f95c48a9cf
miao-pqz <1125583676.qq.com>
2025-07-13 09:28:16 +0800

    05: search filters

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,14 +1,45 @@
+import { getPayload } from "payload";
+
+import { Category } from "@/payload-types";
+import ConfigPromise from "@payload-config";
+
 import { Footer } from "./footer";
 import { Navbar } from "./navbar";
+import { SearchFilters } from "./search-filters";
 
 interface Props {
     children: React.ReactNode;
 };
 
-const Layout = ({ children }: Props) => {
+const Layout = async ({ children }: Props) => {
+    const payload = await getPayload({
+        config: ConfigPromise,
+    });
+
+    const data = await payload.find({
+        collection: "categories",
+        depth: 1, // Populate subcategories, subcategories.[0] will be a type of "Category"
+        pagination: false,
+        where: {
+            parent: {
+                exists: false,
+            },
+        },
+    });
+
+    const formattedData = data.docs.map((doc) => ({
+        ...doc,
+        subcategories: (doc.subcategories?.docs?? []).map((doc) => ({
+            // Because of "depth: 1" we are confident "doc" will be a type of "Category"
+            ...(doc as Category),
+            subcategories: undefined,
+        }))
+    }));
+
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
+            <SearchFilters data={formattedData} />
             <div className="flex-1 bg-[#F4F4F0]">
                 {children}
             </div>
