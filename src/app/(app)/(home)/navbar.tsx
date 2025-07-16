c72a6ecbc3f5d6dcdc5a32731bc998e244a690d2
miao-pqz <1125583676.qq.com>
2025-07-16 15:10:41 +0800

    09: auth states

--- src/app/(app)/(home)/navbar.tsx
+++ src/app/(app)/(home)/navbar.tsx
@@ -5,10 +5,12 @@ import { useState } from "react";
 import { MenuIcon } from "lucide-react";
 import { Poppins } from "next/font/google";
 import { usePathname } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
 
 import { Button } from "@/components/ui/button";
 
 import { cn } from "@/lib/utils";
+import { useTRPC } from "@/trpc/client";
 
 import { NavbarSidebar } from "./navbar-sidebar";
 
@@ -52,6 +54,9 @@ export const Navbar = () => {
     const pathname = usePathname();
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
+    const trpc = useTRPC();
+    const session = useQuery(trpc.auth.session.queryOptions());
+
     return (
         <nav className="h-20 flex border-b justify-between font-medium bg-white">
             <Link href="/" className="pl-6 flex items-center">
@@ -70,25 +75,38 @@ export const Navbar = () => {
                     </NavbarItem>
                 ))}
             </div>
-            <div className="hidden lg:flex">
-                <Button
-                    asChild
-                    variant="secondary"
-                    className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-white hover:bg-pink-400 transition-colors text-lg"
-                >
-                    <Link prefetch href="/sign-in">
-                        Log in
-                    </Link>
-                </Button>
-                <Button
-                    asChild
-                    className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black text-white hover:bg-pink-400 hover:text-black transition-colors text-lg"
-                >
-                    <Link prefetch href="/sign-up">
-                        Start selling
-                    </Link>
-                </Button>
-            </div>
+            {session.data?.user ? (
+                <div className="hidden lg:flex">
+                    <Button
+                        asChild
+                        className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black text-white hover:bg-pink-400 hover:text-black transition-colors text-lg"
+                    >
+                        <Link href="/admin">
+                            Dashboard
+                        </Link>
+                    </Button>
+                </div>
+            ) : (
+                <div className="hidden lg:flex">
+                    <Button
+                        asChild
+                        variant="secondary"
+                        className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-white hover:bg-pink-400 transition-colors text-lg"
+                    >
+                        <Link prefetch href="/sign-in">
+                            Log in
+                        </Link>
+                    </Button>
+                    <Button
+                        asChild
+                        className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black text-white hover:bg-pink-400 hover:text-black transition-colors text-lg"
+                    >
+                        <Link prefetch href="/sign-up">
+                            Start selling
+                        </Link>
+                    </Button>
+                </div>
+            )}
             <div className="flex lg:hidden items-center justify-center">
                 <Button
                     variant="ghost"
--- src/app/(app)/(home)/search-filters/search-input.tsx
+++ src/app/(app)/(home)/search-filters/search-input.tsx
@@ -1,10 +1,14 @@
 "use client";
 
+import Link from "next/link";
 import { useState } from "react";
-import { ListFilterIcon, SearchIcon } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
+import { BookmarkCheckIcon, ListFilterIcon, SearchIcon } from "lucide-react";
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+
+import { useTRPC } from "@/trpc/client";
 
 import { CategoriesSidebar } from "./categories-sidebar";
 
@@ -15,6 +19,9 @@ interface Props {
 export const SearchInput = ({ disabled }: Props) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
+    const trpc = useTRPC();
+    const session = useQuery(trpc.auth.session.queryOptions());
+
     return (
         <div className="flex items-center gap-2 w-full">
             <CategoriesSidebar open={isSidebarOpen} onOpenChange={setIsSidebarOpen} />
@@ -22,7 +29,6 @@ export const SearchInput = ({ disabled }: Props) => {
                 <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500" />
                 <Input className="pl-8" placeholder="Search products" disabled={disabled} />
             </div>
-            {/* TODO: Add categories view all button */}
             <Button
                 variant="elevated"
                 className="size-12 shrink-0 flex lg:hidden"
@@ -30,7 +36,17 @@ export const SearchInput = ({ disabled }: Props) => {
             >
                 <ListFilterIcon />
             </Button>
-            {/* TODO: Add library button */}
+            {session.data?.user && (
+                <Button
+                    asChild
+                    variant="elevated"
+                >
+                    <Link href="/library">
+                        <BookmarkCheckIcon />
+                        Library
+                    </Link>
+                </Button>
+            )}
         </div>
     );
 };
