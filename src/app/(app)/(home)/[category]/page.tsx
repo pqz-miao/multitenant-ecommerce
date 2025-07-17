3f24cd2920ed846bf2c1996339549528e2e2643e
miao-pqz <1125583676.qq.com>
2025-07-17 21:15:57 +0800

    12: filtering products

--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 import { getQueryClient, trpc } from "@/trpc/server";
+import { ProductFilters } from "@/modules/products/ui/components/product-filters";
 import { ProductsList, ProductsListSkeleton } from "@/modules/products/ui/components/products-list";
 
 interface Props {
@@ -18,9 +19,18 @@ const Page = async ({ params }: Props) => {
 
     return (
         <HydrationBoundary state={dehydrate(queryClient)}>
-            <Suspense fallback={<ProductsListSkeleton />}>
-                <ProductsList category={category} />
-            </Suspense>
+            <div className="px-4 lg:px-12 py-8 flex flex-col gap-4">
+                <div className="grid grid-cols-1 lg:grid-cols-6 xl:grid-cols-8 gap-y-6 gap-x-12">
+                    <div className="lg:col-span-2 xl:col-span-2">
+                        <ProductFilters />
+                    </div>
+                    <div className="lg:col-span-4 xl:col-span-6">
+                        <Suspense fallback={<ProductsListSkeleton />}>
+                            <ProductsList category={category} />
+                        </Suspense>
+                    </div>
+                </div>
+            </div>
         </HydrationBoundary>
     );
 };
