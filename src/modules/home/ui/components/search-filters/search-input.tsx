ef59f8536ac76eb4963f943d6f5884ece1326dd2
miao-pqz <1125583676.qq.com>
2025-07-24 10:21:42 +0800

    21: library

--- src/modules/home/ui/components/search-filters/search-input.tsx
+++ src/modules/home/ui/components/search-filters/search-input.tsx
@@ -41,7 +41,7 @@ export const SearchInput = ({ disabled }: Props) => {
                     asChild
                     variant="elevated"
                 >
-                    <Link href="/library">
+                    <Link prefetch href="/library">
                         <BookmarkCheckIcon />
                         Library
                     </Link>
