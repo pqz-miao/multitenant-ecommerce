222686bf4c01b860b159040531d7f9290e8381aa
pqz-miao <pangqingzhu2024@gmail.com>
2025-12-21 20:21:42 +0800

    01: setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+  return (
+    <Button variant="ghost" className="text-red-500">
+      Hello World
+    </Button>
+  );
+};
+
+export default Page;
