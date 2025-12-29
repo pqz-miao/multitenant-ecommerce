14936c93283fad84bfab0dc3680c4557e467dfb7
pqz-miao <pangqingzhu2024@gmail.com>
2025-12-29 15:43:48 +0800

    08: authentication

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -9,5 +9,11 @@ export const Users: CollectionConfig = {
   fields: [
     // Email added by default
     // Add more fields as needed
+    {
+      name: "username",
+      type: "text",
+      required: true,
+      unique: true,
+    },
   ],
 }
