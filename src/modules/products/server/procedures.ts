d368b15f97d6b6cede3e90154006695e9d1ff469
pqz-miao <pangqingzhu2024@gmail.com>
2025-12-31 20:14:57 +0800

    12: filtering products

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -9,10 +9,24 @@ export const productsRouter = createTRPCRouter({
         .input(
             z.object({
                 category: z.string().nullable().optional(),
+                minPrice: z.string().nullable().optional(),
+                maxPrice: z.string().nullable().optional(),
             })
         )
         .query(async ({ ctx, input }) => {
             const where: Where = {};
+
+            if (input.minPrice) {
+                where.price = {
+                    greater_than_equal: input.minPrice,
+                };
+            }
+
+            if (input.maxPrice) {
+                where.price = {
+                    less_than_equal: input.maxPrice,
+                };
+            }
 
             if (input.category) {
                 const categoriesData = await ctx.db.find({
@@ -42,11 +56,11 @@ export const productsRouter = createTRPCRouter({
                     subcategorySlugs.push(
                         ...parentCategory.subcategorySlugs?.map((subcategory) => subcategory.slug)
                     );
-                }
 
-                where["category.slug"] = {
-                    in: [parentCategory.slug, ...subcategorySlugs],
-                };
+                    where["category.slug"] = {
+                        in: [parentCategory.slug, ...subcategorySlugs],
+                    };
+                }
             }
 
             const data = await ctx.db.find({
